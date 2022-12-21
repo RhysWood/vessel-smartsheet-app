c1b305b53db8df64d13b85850e45c88cd0e86d7d
Rhys <rhyswoody@gmail.com>
2022-12-21 15:01:50 -0800

    working app

--- index.js
+++ index.js
@@ -27,53 +27,75 @@ const smartsheet = client.createClient({
     logLevel: 'info'
   });
 
-smartsheet.sheets.getSheet({id: process.env.sheet_id, includeAll: true})
-.then(function(sheetInfo) {
-    let rowIdArray = [];
-    sheetInfo.rows.forEach(function(row) {
-        rowIdArray.push(row.id);
+
+
+  const rowIDsarray = async (rows) => {
+      let array = [];
+      for (let i=0; i<rows.length; i++) {
+          array.push(rows[i].id);
+      }
+      return array;
+  }
+
+const getRows = async () => {
+    let results = await (await smartsheet.sheets.getSheet({id: process.env.sheet_id, includeAll: true}))
+    let rowIDs = rowIDsarray(results.rows);
+    // returns an array of row IDs
+    return rowIDs;
+}
+
+const getAttachment = async (rowId) => {
+    let result = await (await smartsheet.sheets.getRowAttachments({sheetId: process.env.sheet_id, rowId: rowId}));
+    //don't use 0 - loop through attachments FUTURE BUG
+    let attachment = await (await smartsheet.sheets.getAttachment({sheetId: process.env.sheet_id, attachmentId: result.data[0].id}));
+    return attachment;
+}
+const getAttachmentFile = async (attachment) => {
+    const url = attachment.url;
+    return new Promise((resolve, reject) => {
+         https.get(url, (res) => {
+        return resolve(res);
+        })})
+    }
+
+const writeStream = async (res) => {
+    const path = "downloaded-image.pdf";
+    const writeStream = fs.createWriteStream(path);
+
+    res.pipe(writeStream);
+
+    writeStream.on("finish", () => {
+        writeStream.close();
+        console.log("Download Completed!");
     })
-    return rowIdArray;
-}).then(function(rowIdArray) {
-    rowIdArray.forEach(function(rowId) {
-        smartsheet.sheets.getRowAttachments({sheetId: process.env.sheet_id, rowId: rowId})
-        .then(function(attachment){
-            const attachment_id = attachment.data[0].id;
-            return smartsheet.sheets.getAttachment({sheetId: process.env.sheet_id, attachmentId: attachment_id})
-        }).then(function(attachment) {
-            const url = attachment.url;
-            https.get(url, (res) => {
-            const path = "downloaded-image.pdf";
-            const writeStream = fs.createWriteStream(path);
-        
-           res.pipe(writeStream);
-        
-           writeStream.on("finish", () => {
-              writeStream.close();
-              console.log("Download Completed!");
-           })
-        })
-        })
-        .then(function() {
-            const sendEmail = function(){
-            transporter.sendMail(mailOptions, function(error, info){
-                if (error) {
-                  console.log(error);
-                } else {
-                  console.log('Email sent: ' + info.response);
-                }
-              });
-            }
-            setTimeout(sendEmail, 1500);
-        }).then(function() {
-            console.log("Email sent!");
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-    })
-}).then(function() {
-    console.log("All done!");
-})
+}
+
+const sendEmail = () => {
+    const sendEmail = function(){
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    }
+    setTimeout(sendEmail, 1500);
+}
+
+const LoopIdsAndGetAttachments = async () => {
+    let rowIDs = await getRows();
+    for (let i=0; i<rowIDs.length; i++) {
+        let attachment = await getAttachment(rowIDs[i]);
+        //Up to this point, we are getting all the attachments 
+        let res = await getAttachmentFile(attachment)
+        await writeStream(res)
+
+        let email = sendEmail()
+        email
+    }
+}
+
+LoopIdsAndGetAttachments();
 
 
